288f64ab350d4152b6fc28a187d28e20c750147a
Jose Alfredo <alfredo_rc@tlaxiaco.tecnm.mx>
2024-10-13 23:48:29 -0600

    Hello Wprld

---
+++ public/contracts/hello_world.js
@@ -0,0 +1,4 @@
+// src/contracts/hello_world.js
+export const helloWorld = () => {
+    console.log("Hello, world!");
+};
---
+++ public/hello_world.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloWorld = void 0;
+// src/contracts/hello_world.js
+var helloWorld = function () {
+    console.log("Hello, world!");
+};
+exports.helloWorld = helloWorld;
