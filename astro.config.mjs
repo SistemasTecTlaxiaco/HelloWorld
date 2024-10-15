a6d2aca86c6201b0ab888e0ba8482ea52be77299
Jose Alfredo <alfredo_rc@tlaxiaco.tecnm.mx>
2024-10-14 18:27:36 -0600

    Se implemento la wallet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server', // o 'hybrid'
-  // otras configuraciones...
+  output: 'server',
+  adapter: node({
+    mode: 'standalone' // Puedes usar 'standalone' o 'middleware' según tus necesidades
+  }),
 });
