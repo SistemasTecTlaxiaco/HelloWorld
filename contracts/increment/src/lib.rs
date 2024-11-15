b87a44a740572726795c81486552b79c68a2bafc
Jose Alfredo <alfredo_rc@tlaxiaco.tecnm.mx>
2024-11-15 15:09:04 -0600

    Haciendo cambios al contrato inteligente

--- contracts/increment/src/lib.rs
+++ contracts/increment/src/lib.rs
@@ -9,9 +9,9 @@ pub struct IncrementContract;
 #[contractimpl]
 impl IncrementContract {
     /// Increment increments an internal counter, and returns the value.
-    pub fn increment(env: Env) -> u32 {
+    pub fn increment(env: Env) -> i32 {
         // Get the current count.
-        let mut count: u32 = env.storage().instance().get(&COUNTER).unwrap_or(0); // If no value set, assume 0.
+        let mut count: i32 = env.storage().instance().get(&COUNTER).unwrap_or(0); // If no value set, assume 0.
         log!(&env, "count: {}", count);
 
         // Increment the count.
@@ -20,15 +20,7 @@ impl IncrementContract {
         // Save the count.
         env.storage().instance().set(&COUNTER, &count);
 
-        // The contract instance will be bumped to have a lifetime of at least 100 ledgers if the current expiration lifetime at most 50.
-        // If the lifetime is already more than 100 ledgers, this is a no-op. Otherwise,
-        // the lifetime is extended to 100 ledgers. This lifetime bump includes the contract
-        // instance itself and all entries in storage().instance(), i.e, COUNTER.
-        env.storage().instance().extend_ttl(50, 100);
-
-        // Return the count to the caller.
+        // Return the new count.
         count
     }
 }
-
-mod test;
